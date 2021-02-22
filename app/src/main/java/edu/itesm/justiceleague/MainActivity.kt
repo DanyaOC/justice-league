0d95766d9630038fd608599ebc1da82ffab4972d
DanyaOC <36273088+DanyaOC@users.noreply.github.com>
2021-02-22 03:07:26 -0600

    commit2 - up to date 19 feb

--- app/src/main/java/edu/itesm/justiceleague/MainActivity.kt
+++ app/src/main/java/edu/itesm/justiceleague/MainActivity.kt
@@ -2,10 +2,43 @@ package edu.itesm.justiceleague
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
+import android.view.View
+import android.widget.EditText
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    val justiceleague = mutableListOf<Superhero>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        init()
+    }
+
+    fun init(){
+        editTextTextPersonName.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(p0: Editable?) {
+                button.text = p0.toString()
+            }
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+        })
+    }
+
+    fun save(view: View){
+        var superhero = Superhero(editTextTextPersonName.text.toString())
+        superhero.phone = editTextPhone.text.toString()
+        superhero.email = editTextTextEmailAddress.text.toString()
+        superhero.age = editTextNumberSigned.text.toString().toInt()
+
+        justiceleague.add(superhero)
+        Log.i("edu.itesm.justiceleague", justiceleague.toString())
+        Log.i("edu.itesm.justiceleague", justiceleague.sumBy { it.age }.toString())
+
     }
 }
